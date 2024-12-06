220ff4d8b09a9796fd1a65895260a0cf1dcf1491
roopang10 <roopang10@gmail.com>
2024-12-06 20:41:54 +0000

    add skeleton code for spt

--- src/vm/spt.c
+++ src/vm/spt.c
@@ -1,31 +1,31 @@
 #include "vm/spt.h"
 
-init_spt(struct hash *spt, void *user_page, void *kernel_page)
+void init_spt(struct hash *spt, void *user_page, void *kernel_page)
 {
 
 }
 
-init_spt_with_zero(struct hash *spt, void *user_page) 
+void init_spt_with_zero(struct hash *spt, void *user_page) 
 {
 
 }
 
-init_spt_for_frame(struct hash *spt, void *user_page, void *kernel_page)
+void init_spt_for_frame(struct hash *spt, void *user_page, void *kernel_page)
 {
 
 }
 
-init_spt_for_file(struct hash *spt, void *user_page, struct file *file, off_t ofs, uint32_t read_bytes, uint32_t zero_bytes, bool writable) 
+void init_spt_for_file(struct hash *spt, void *user_page, struct file *file, off_t ofs, uint32_t read_bytes, uint32_t zero_bytes, bool writable) 
 {
 
 }
 
-vm_get_spt(struct hash *spt, void *user_page)
+struct spt *vm_get_spt(struct hash *spt, void *user_page)
 {
 
 }
 
-vm_page_delete(struct hash *spt, struct spt *s)
+void vm_spt_page_delete(struct hash *spt, struct spt *s)
 [
 
 ]
